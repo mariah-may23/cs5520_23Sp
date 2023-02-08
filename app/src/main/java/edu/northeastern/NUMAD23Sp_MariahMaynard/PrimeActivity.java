b626f6f7b7c8bb3bfda84d7570152c5426818f94
Mariah Maynard <98000652+mariah-may23@users.noreply.github.com>
2023-02-08 12:16:29 -0500

    rotation fixed

--- app/src/main/java/edu/northeastern/NUMAD23Sp_MariahMaynard/PrimeActivity.java
+++ app/src/main/java/edu/northeastern/NUMAD23Sp_MariahMaynard/PrimeActivity.java
@@ -1,10 +1,12 @@
 package edu.northeastern.NUMAD23Sp_MariahMaynard;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.TextView;
 
@@ -14,6 +16,8 @@ public class PrimeActivity extends AppCompatActivity {
     private Handler textHandler = new Handler();
     boolean flag = false;
     int INFINITY = Integer.MAX_VALUE;
+    private int current_prime;
+    private int current_count = 3;
 
 
     @Override
@@ -24,6 +28,16 @@ public class PrimeActivity extends AppCompatActivity {
         current_number = findViewById(R.id.current_number);
         System.out.println("creating");
 
+        if(savedInstanceState != null) {
+            current_prime = savedInstanceState.getInt("prime");
+            prime.setText(String.valueOf(current_prime));
+
+            current_count = savedInstanceState.getInt("count");
+            System.out.println("CURRENT COUNT" + current_count);
+            current_number.setText(String.valueOf(current_count));
+            runnableThread primeThread = new runnableThread();
+            new Thread(primeThread).start();
+        }
     }
 
     public void here(View v) {
@@ -42,14 +56,16 @@ public class PrimeActivity extends AppCompatActivity {
         @Override
         public void run() {
             while (!flag) {
-                for (int i = 3; i <= INFINITY; i += 2) { //start at 3 and increment forever by adding 2
+                for (int i = current_count; i <= INFINITY; i += 2) { //start at 3 and increment forever by adding 2
                     int finalI = i;
+                    current_count = finalI;
                     textHandler.post(new Runnable() {
                         @SuppressLint("SetTextI18n")
                         @Override
                         public void run() {
                             System.out.println("BEFORE");
                             current_number.setText("Number Being Checked: " + finalI); // set the current number being checked
+
                             for (int j = 2; j < finalI; j++) {
                                 if (finalI % j == 0) { // the number should only be divisible by one and itself
                                     flag = true;
@@ -61,6 +77,7 @@ public class PrimeActivity extends AppCompatActivity {
                             if (!flag) { // after checking, if the fag hasn't changed, we know the number is prime
                                 String last_prime = String.valueOf(finalI);
                                 prime.setText(last_prime);
+                                current_prime = finalI;
                             }
                             System.out.println("AFTER");
                             flag = false;
@@ -78,4 +95,14 @@ public class PrimeActivity extends AppCompatActivity {
 
 
     }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt("prime", current_prime);
+        outState.putInt("count", current_count);
+    }
+
+
 }
