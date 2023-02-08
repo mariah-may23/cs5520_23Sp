e15b6962f7b28e0b1adc9124c7d68019045d89e1
Mariah Maynard <98000652+mariah-may23@users.noreply.github.com>
2023-02-08 12:54:06 -0500

    terminating

--- app/src/main/java/edu/northeastern/NUMAD23Sp_MariahMaynard/PrimeActivity.java
+++ app/src/main/java/edu/northeastern/NUMAD23Sp_MariahMaynard/PrimeActivity.java
@@ -15,6 +15,7 @@ public class PrimeActivity extends AppCompatActivity {
     TextView current_number;
     private Handler textHandler = new Handler();
     boolean flag = false;
+    boolean terminate = false;
     int INFINITY = Integer.MAX_VALUE;
     private int current_prime;
     private int current_count = 3;
@@ -40,58 +41,74 @@ public class PrimeActivity extends AppCompatActivity {
         }
     }
 
-    public void here(View v) {
-        System.out.println("clicked");
+    public void terminate(View v) {
+        System.out.println("terminating");
+        terminate = true;
+      
+
+
     }
 
     public void runFindPrimes(View v) {
-        System.out.println("HERE");
+     
+
         runnableThread primeThread = new runnableThread();
         new Thread(primeThread).start();
-        System.out.println("HERE");
+
     }
 
     class runnableThread implements Runnable {
+        private volatile Boolean stop = false;
 
         @Override
         public void run() {
-            while (!flag) {
-                for (int i = current_count; i <= INFINITY; i += 2) { //start at 3 and increment forever by adding 2
-                    int finalI = i;
-                    current_count = finalI;
-                    textHandler.post(new Runnable() {
-                        @SuppressLint("SetTextI18n")
-                        @Override
-                        public void run() {
-                            System.out.println("BEFORE");
-                            current_number.setText("Number Being Checked: " + finalI); // set the current number being checked
 
-                            for (int j = 2; j < finalI; j++) {
-                                if (finalI % j == 0) { // the number should only be divisible by one and itself
-                                    flag = true;
-                                    break;
-                                }
+
+            System.out.println("STOP" + stop);
+            for (int i = current_count; i <= INFINITY; i += 2) { //start at 3 and increment forever by adding 2
+                if(stop){
+                    break;
+                }
+                int finalI = i;
+                current_count = finalI;
+                textHandler.post(new Runnable() {
+                    @SuppressLint("SetTextI18n")
+                    @Override
+                    public void run() {
+                       // System.out.println("BEFORE");
+                        current_number.setText("Number Being Checked: " + finalI); // set the current number being checked
+
+                        for (int j = 2; j < finalI; j++) {
+                            if (finalI % j == 0) { // the number should only be divisible by one and itself
+                                flag = true;
+                                break;
                             }
-                            System.out.println("next");
-
-                            if (!flag) { // after checking, if the fag hasn't changed, we know the number is prime
-                                String last_prime = String.valueOf(finalI);
-                                prime.setText(last_prime);
-                                current_prime = finalI;
-                            }
-                            System.out.println("AFTER");
-                            flag = false;
-
                         }
-                    });
-                    try {
-                        Thread.sleep(1000); //Makes the thread sleep or be inactive for 10 seconds
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+                     //   System.out.println("next");
+
+                        if (!flag) { // after checking, if the fag hasn't changed, we know the number is prime
+                            String last_prime = String.valueOf(finalI);
+                            prime.setText(last_prime);
+                            current_prime = finalI;
+                        }
+                     //   System.out.println("AFTER");
+                        flag = false;
+                        stop = terminate;
+                        System.out.println(stop);
+
                     }
+                });
+                try {
+                    Thread.sleep(1000); //Makes the thread sleep or be inactive for 10 seconds
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
+
         }
+
+
+
 
 
     }
