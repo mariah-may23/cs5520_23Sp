aad33bd00a1f84db94c412c543c7eb95b5450221
Mariah Maynard <98000652+mariah-may23@users.noreply.github.com>
2023-03-18 10:05:57 -0400

    added reset button, alert dialogs for location tracking

--- app/src/main/java/edu/northeastern/NUMAD23Sp_MariahMaynard/LocationActivity.java
+++ app/src/main/java/edu/northeastern/NUMAD23Sp_MariahMaynard/LocationActivity.java
@@ -1,14 +1,90 @@
 package edu.northeastern.NUMAD23Sp_MariahMaynard;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LocationActivity extends AppCompatActivity {
+
+    TextView latitude;
+    TextView longitude;
+
+    LocationManager manager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+        latitude = findViewById(R.id.textView6);
+        longitude = findViewById(R.id.textView8);
+
+        // instance to interact with location
+        manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        //ask permission
+        if (ActivityCompat.checkSelfPermission(LocationActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Please Allow US to Track Your Location");
+            builder.setMessage("This activity shows your current latitude and longitude. To work, it needs you to grant location access");
+            builder.setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    ActivityCompat.requestPermissions(LocationActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 100);
+                }
+            });
+            builder.show();
+        } else {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Currently Tracking Your Location");
+            builder.setMessage("You have given this activity permission to show your current latitude and longitude.");
+            builder.setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    ActivityCompat.requestPermissions(LocationActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 100);
+                }
+            });
+        }
+        LocationListener listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                latitude.setText(String.valueOf(location.getLatitude()));
+                longitude.setText(String.valueOf(location.getLongitude()));
+
+            }
+        };
+
+        manager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,listener);
+
     }
+
+    public void resetLocation(View v){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Resetting Location");
+        builder.setMessage("This action will reset your total distance.");
+        builder.setPositiveButton("Contnue",);
+    }
+
 }
+
+
+
+
+
+
+
+
+
